04abcbe14e9586dad7e2c6a9b842cfb1bc1e1452
wei-tsung-cheng <william.cheng@kkday.com>
2022-06-04 23:12:51 +0800

    f5

--- ci.sh
+++ ci.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 set -e
-xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=$$eric/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
+xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
